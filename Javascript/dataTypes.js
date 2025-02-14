bb689f312f5173670a55108b8717d35e1bbd3f0d
Md Parvez Ahamed <devparvez23@gmail.com>
2025-02-14 15:36:22 +0600

    javascript data type complete

---
+++ Javascript/dataTypes.js
@@ -0,0 +1,39 @@
+
+
+let country = "Bangladesh";
+
+// JavaScript Data Types
+
+// String
+let name = "Md Parvez Ahamed";
+let language = 'javascript'
+// console.log(typeof name);
+
+// number
+/***
+ * Number Data types integer and Float value
+ * 
+ * 
+ * */ 
+let age = 20 ; // integer
+let price = 5.58 // float number
+// console.log(typeof price);
+// console.log(typeof age);
+
+// Booleans Data type (true or false)
+let userLogin = true;
+let hasAccessInAdminPlane = false
+console.log(typeof userLogin);
+
+// undefine Data Type : undefine means variable declare but value not assign
+
+let joinPicnic
+
+// null
+
+
+
+// Template literals
+
+let message = `my name is ${name} . My favorite programming language is ${language}`
+// console.log(message);
